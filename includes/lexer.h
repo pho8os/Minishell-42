79791e0b7d355912b5977a90343cf6d97ea70590
Yassin Ettabaa <yassin.ettabaa1998@gmail.com>
2023-03-03 05:51:25 +0100

    chore : lexing

--- includes/lexer.h
+++ includes/lexer.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: absaid <absaid@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yettabaa <yettabaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 19:32:37 by absaid            #+#    #+#             */
-/*   Updated: 2023/03/03 01:55:40 by absaid           ###   ########.fr       */
+/*   Updated: 2023/03/03 05:05:17 by yettabaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,17 @@ typedef struct s_token
 	char			*token;
 	struct s_token	*next;
 } t_token;
+#define TOKEN_WORD  0
+#define TOKEN_PIPE  1
+#define TOKEN_OR  2
+#define TOKEN_AND  3
+#define TOKEN_REDIRECT_INPUT  4
+#define TOKEN_REDIRECT_OUTPUT  5
+#define TOKEN_HERE_DOC  6
+#define TOKEN_REDIRECT_APPEND  7
+#define SINGLEQ  8
+#define DOUBLEQ  9
+
 
 
 #endif
--- lexing/lexer.c
+++ lexing/lexer.c
@@ -3,27 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: absaid <absaid@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yettabaa <yettabaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 19:30:23 by absaid            #+#    #+#             */
-/*   Updated: 2023/03/03 02:33:24 by absaid           ###   ########.fr       */
+/*   Updated: 2023/03/03 05:19:04 by yettabaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lexer.h"
-
-bool special(char c, char next)
+t_token	*newtok(int type, char *token)
 {
-	bool ret;
+	t_token	*node;
+
+	node = (t_token *)malloc(sizeof (t_token));
+	if (!node)
+		return (NULL);
+	node->type = type;
+	node->token = token;
+	node->next = NULL;
+	return (node);
+}
+
+char *gettok(char *cmdl, int ind, int *i)
+{
+	int start = ind;
+	int end = ind + 1;
+	
+	if(start)
+		while(/*cmdl[start]* machi special && */ start >=0)
+			start--;		
+	while(cmdl[end] != *cmdl && cmdl[end])
+		end++;
+	if(!cmdl[end])
+		write(2, "error", 5); // TODO : exit the process;
+	while(cmdl[end] /*and machi special*/)
+		end++;
+	return(*i = end, ft_substr(cmdl, start, end - start));
+}
+
+t_token *test(char *cmdl, int ind, int *i)
+{
+	if (cmdl[ind] == '\'')
+		return(newtok(SINGLEQ, gettok(cmdl, ind, i)));
+	else if (cmdl[ind] == '|' && cmdl[ind + 1] != '|')
+		return(newtok(TOKEN_PIPE, ft_strdup("|")));
+	else if (cmdl[ind] == '<' && cmdl[ind + 1] != '<')
+		return(newtok(TOKEN_REDIRECT_INPUT, ft_strdup("<")));
+	else if (cmdl[ind] == '>' && cmdl[ind + 1] != '>')
+		return(newtok(TOKEN_REDIRECT_OUTPUT ,ft_strdup(">")));
+	else if (cmdl[ind] == '&' && cmdl[ind + 1] == '&')
+		return(*i = ++ind, newtok(TOKEN_AND ,ft_strdup( "&&")));
+	else if (cmdl[ind] == '|' && cmdl[ind + 1] == '|')
+		return(*i = ++ind, newtok(TOKEN_OR ,ft_strdup("||")));
+	else if (cmdl[ind] == '<' && cmdl[ind + 1] == '<')
+		return(*i = ++ind, newtok(TOKEN_HERE_DOC ,ft_strdup("<<")));
+	else if (cmdl[ind] == '>' && cmdl[ind + 1] == '>')
+		return(*i = ++ind, newtok(TOKEN_HERE_DOC ,ft_strdup(">>")));
 }
 
 t_token *lexer(char *cmdline)
 {
+	t_token *head;
 	int	i;
 	
 	i = -1;
 	while(cmdline[++i])
 	{
-		
+		ft_lstadd_back(&head, test(cmdline, i, &i));
 	}
 }
